f1943486c214554167a5ebf653ddcaefa232fa82
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-19 22:21:00 +0300

    feat(TimerAdd): logic add new event. WIP

--- src/panels/TimerAdd.tsx
+++ src/panels/TimerAdd.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import bridge from "@vkontakte/vk-bridge";
 import {
   Panel,
   PanelHeader,
@@ -8,25 +9,47 @@ import {
   Group,
   List,
   Header,
+  Cell,
 } from "@vkontakte/vkui/dist";
 
+import { ValuesPanelId } from "../shared/types";
+import { PanelId } from "../shared/consts";
 import { getStringDate } from "../shared/utils";
 
 type Props = {
-  id: string;
+  id: ValuesPanelId;
+  go: (id: ValuesPanelId) => void;
+  nextTimeId: number;
 };
 
-const Timer: React.FC<Props> = ({ id }: Props) => {
+const Timer: React.FC<Props> = ({ id, go, nextTimeId }: Props) => {
   const [date, setDate] = useState(0);
   const [time, setTime] = useState(0);
+  const [nameEvent, setNameEvent] = useState("");
   const [isVisibilityTime, setVisibilityTime] = useState(false);
+  const [newTimeData, setNewTimeData] = useState("");
 
   if (isNaN(time)) {
     setTime(0);
   }
 
-  const isBeforeDate = (date + time) < Date.now();
-  const stringDate = getStringDate(date, time);
+  useEffect(() => {
+    async function sendTimeData() {
+      if (newTimeData) {
+        await bridge.send("VKWebAppStorageSet", {
+          key: `${nextTimeId}`,
+          value: newTimeData,
+        });
+
+        go(PanelId.TIMERS_LIST);
+      }
+    }
+
+    sendTimeData();
+  }, [newTimeData]);
+
+  const isBeforeDate = date + time < Date.now();
+  const stringDate = getStringDate(date + time);
 
   return (
     <Panel id={id}>
@@ -38,6 +61,7 @@ const Timer: React.FC<Props> = ({ id }: Props) => {
             top="Дата"
             name="date"
             onChange={(evt) => setDate(evt.target.valueAsNumber)}
+            required
           />
 
           <Button onClick={() => setVisibilityTime(!isVisibilityTime)}>
@@ -52,14 +76,35 @@ const Timer: React.FC<Props> = ({ id }: Props) => {
               onChange={(evt) => setTime(evt.target.valueAsNumber)}
             />
           )}
+          <Input
+            placeholder="Название события"
+            onChange={(evt) => setNameEvent(evt.target.value)}
+            status="default"
+            required
+          ></Input>
+          <Button
+            size="xl"
+            onClick={(evt) => {
+              console.log(evt);
+              setNewTimeData(
+                  `${isBeforeDate ? "-" : "+"}${date + time},${nameEvent}`
+              );
+            }}
+          >
+            {"Сохранить событие"}
+          </Button>
         </FormLayout>
       </Group>
 
       <Group>
-        {(!!stringDate && isBeforeDate) && <Header mode="secondary">Прошло с даты:</Header>}
-        {(!!stringDate && !isBeforeDate) && <Header mode="secondary">До даты осталось:</Header>}
+        {!!stringDate && isBeforeDate && (
+          <Header mode="secondary">Прошло с даты:</Header>
+        )}
+        {!!stringDate && !isBeforeDate && (
+          <Header mode="secondary">До даты осталось:</Header>
+        )}
         <List>
-          {getStringDate(date, time)}
+          <Cell>{getStringDate(date + time)}</Cell>
         </List>
       </Group>
     </Panel>
